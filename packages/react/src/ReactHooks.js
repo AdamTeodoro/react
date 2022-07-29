c7bb2f3f3460517b46a7f06a4db6df89a903e849
AdamTeodoro <66885644+AdamTeodoro@users.noreply.github.com>
2022-07-28 23:16:17 -0300

    update useEffect
    remove useEffect, add onInitComponent, add subscribeTo

--- packages/react/src/ReactHooks.js
+++ packages/react/src/ReactHooks.js
@@ -97,13 +97,42 @@ export function useRef<T>(initialValue: T): {|current: T|} {
   const dispatcher = resolveDispatcher();
   return dispatcher.useRef(initialValue);
 }
+/**
+ * This code is removed 😝
+ * Remember:  "Não existem BONS CÓDIGOS,
+ * só existem códigos que ainda não foram melhorados"
+ */
+// export function useEffect(
+//   create: () => (() => void) | void,
+//   deps: Array<mixed> | void | null,
+// ): void {
+//   const dispatcher = resolveDispatcher();
+//   return dispatcher.useEffect(create, deps);
+// }
 
-export function useEffect(
+/**
+ * In the initial component run this,
+ * it's a best practice to use the name relative
+ * to the work the function is going to do.
+ */
+ export function onInitComponent(
   create: () => (() => void) | void,
-  deps: Array<mixed> | void | null,
-): void {
+) {
   const dispatcher = resolveDispatcher();
-  return dispatcher.useEffect(create, deps);
+  return dispatcher.useEffect(create, null);
+}
+
+/**
+ * looking at the good practice applied in the previous function,
+ * we realize that function "useEffect" does two things,
+ * and remember the single responsibility principle S2,
+ * which is also a good practice.
+ */
+export function subscribeTo(
+  deps: Array<mixed>
+) {
+  const dispatcher = resolveDispatcher();
+  return dispatcher.useEffect(() => {}, deps);
 }
 
 export function useInsertionEffect(
